68defc59981f7c13d81ae3d572eecb2c111bcde1
Daniel Bresnahan <daniel.p.bresnahan@gmail.com>
2019-05-25 09:03:39 -0500

    Worked on updated Hexagon class

--- boardPieces/NewHexagon.py
+++ boardPieces/NewHexagon.py
@@ -8,6 +8,7 @@ New Hexagon for optimized generation and storage
 """
 from pygame import sprite
 from pygame import image
+import math
 
 """
 New hexagon class emphasises seperate hexagon coordinates, as apposed to 
@@ -24,11 +25,15 @@ class Hexagon(sprite.Sprite):
         super(Hexagon, self).__init__()
         
         self.q, self.r, self.s = q, r, s
+        
+        #Size is the distance to the corner
         self.Size = Size
+        
         self.SpriteSet = [image.load(x) for x in SpriteSet]
         
         self.index = 0
         self.Sprite = self.SpriteSet[self.index]
+        
      
     """
     Call this to check cube coordinates of hexagon
@@ -70,9 +75,136 @@ class Hexagon(sprite.Sprite):
     def hex_length(hexagon):
         return (abs(hexagon.q) + abs(hexagon.r) + abs(hexagon.s)) // 2
 
-    def hex_distance(a, b):
-        return hex_length(hex_subtract(a, b))
+    def hex_distance(self, a, b):
+        return self.hex_length(self.hex_subtract(a, b))
+    
+    """
+    This function deals with decimal coordinates
+    Hexagon grid can only handle integer coordinates
+    This function will transform decimal into integer by rounding
+    
+    However when rounding there is no quarentee that q + r + s = 0 after rounding
+    
+    to deal with these descripencies this algorithm finds the component with largest decimal
+    and does some algebra to force it to equal what it should be to satisfy q + r + s = 0
+    
+    returns tuple of coordinates
+    """
+    
+    def cube_round(hexagon):
+        
+        round_q = int(round(hexagon.q))
+        round_r = int(round(hexagon.r))
+        round_s = int(round(hexagon.s))
+        
+        q_diff = abs(round_q - hexagon.q)
+        r_diff = abs(round_r - hexagon.r)
+        s_diff = abs(round_s - hexagon.s)
+        
+        if q_diff > r_diff and q_diff > s_diff:
+            round_q = -round_r - round_s
+        else:
+            if r_diff > s_diff:
+                round_r = -round_q - round_s
+            else:
+                round_s = -round_q - round_r
+        return (round_q, round_r, round_s)
+    
+    """
+    LERP or linear interpolation 
+    is a method that allows you to find some point on a line that represents a ratio
+    classically this would be a line mid point calculator with a t value of 0.5
+    
+    used in line drawing
+    """
+    
+    def LERP(Point1, Point2, t):
+        return Point1 + (Point2 - Point1) * t
+    
+    """
+    This utilizes the LERP function to find some point on the line between 2 hexes
+    would represent a midpoint with a t value of 0.5
+    
+    returns a tuple of coordinates
+    """
+    
+    def hexagon_LERP(self, Hex1, Hex2, t):
+        return(self.LERP(Hex1.q, Hex2.q, t),
+               self.LERP(Hex1.r, Hex2.r, t),
+               self.LERP(Hex1.s, Hex2.r, t))
+    
+    
+    
+    """
+    This function will actually draw the line between two hexes
+    returns a list of hexes on the line
+    """
+    
+    def hex_linedraw(self, Hex1, Hex2):
+        
+        N = self.hex_distance(Hex1, Hex2)
+        
+        results = []
+        step = 1.0 / max(N, 1)
+        
+        for i in range(0, N + 1):
+            results.append(self.hex_round(self.hexagon_LERP(Hex1, Hex2, step * i)))
+        return results
+    
+    """
+    These two functions are in charge of finding the neighbors
+    """
 
+    
+    def getNeighbours(self):
+        
+        NeighborDirections = [(self.q + 1, self.r -1, self.s), 
+                              (self.q + 1, self.r, self.s -1), 
+                              (self.q, self.r + 1, self.s -1), 
+                              (self.q -1, self.r + 1, self.s), 
+                              (self.q -1, self.r , self.s + 1), 
+                              (self.q, self.r -1, self.s + 1) ]
+        
+        return NeighborDirections
+    
+    
+    def IsNeighbour(self, Hexagon):
+        
+        if (Hexagon.q, Hexagon.r, Hexagon.s) in self.getNeighbours:
+            return True
+        else:
+            return False
+    
+    
+    """
+    Method for turning the map of hex coordinates into pixel coordinates
+    works by defining basis vector and multiplying it be the number of hexagons. 
+    
+    x = 3/2 * hex.q * scalar y = sqrt(3)/2 * hex.r * scalar
+    
+    this method will find the pixel middle of a hexagon
+    
+    Returns a list of Tuples, where each tuple is a corner coordinate
+    """
+    
+    def hexagon_to_Pixel(self, hexagon):
+        
+        x = self.Size * (     3/2 * hexagon.q                               )
+        y = self.Size * (math.sqrt(3)/2 * hex.q  +  math.sqrt(3) * hexagon.r)
+        
+        cornerList = []
+        
+        for i in range(6):
+            
+            angle_deg = 60 * i - 30
+            angle_rad = round(math.pi / 180 * angle_deg, 2)
+            
+            cornerList.append(round(x + (self.Size * math.cos(angle_rad)), 2), 
+                              round(y + (self.Size * math.sin(angle_rad)), 2))
+        
+        return cornerList
+        
+    
     """
     This method is in charge of updating the current sprite
     with every update of the game
---
+++ newBoard.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 20:09:07 2019
+
+@author: Daniel
+"""
+
+import math
+from boardPieces.NewHexagon import Hexagon
+import random
+
+class Board:
